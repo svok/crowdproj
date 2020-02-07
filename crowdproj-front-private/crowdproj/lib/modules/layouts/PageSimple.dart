7ea25aa4ceab44baf59a1f21ab485bacda1e00fa
Sergey Okatov <sokatov@gmail.com>
2020-02-07 22:25:31 +0500

    Drawer pangel as menu

--- crowdproj-front-private/crowdproj/lib/modules/layouts/PageSimple.dart
+++ crowdproj-front-private/crowdproj/lib/modules/layouts/PageSimple.dart
@@ -1,6 +1,12 @@
+import 'package:crowdproj/common/AppSession.dart';
+import 'package:crowdproj/modules/auth/AuthService.dart';
+import 'package:crowdproj/modules/auth/widgets/MenuUserHead.dart';
+import 'package:crowdproj/translations/AuthLocalizations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:crowdproj/widgets/menu/MenuWidget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class PageSimple extends StatelessWidget {
   PageSimple({
@@ -17,9 +23,32 @@ class PageSimple extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 //    AppSession.get.routes.setWindow(context);
+    final _auth = AppSession.get.authService;
     return Scaffold(
         appBar: MenuWidget(
           title: title,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              MenuUserHead(),
+              Consumer<AuthService>(builder: (context, _auth, child) {
+                print("PAGE SIMPLE: ${_auth.runtimeType} -- ${child?.runtimeType}");
+                final _user = _auth.currentUser;
+                if (!_auth.isAuthenticated() || _user == null)
+                  return Container();
+                return ListTile(
+                    leading: Icon(FontAwesomeIcons.signOutAlt),
+                    title:
+                        new Text(AuthLocalizations.of(context).signoutFor("")),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                      _auth.signOut();
+                    });
+              }),
+            ],
+          ),
+          semanticLabel: "Semantic Label",
         ),
         body: body,
         floatingActionButton: floatingActionButton);
