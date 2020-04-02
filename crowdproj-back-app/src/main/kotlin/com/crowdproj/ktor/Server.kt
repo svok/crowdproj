3f4e8b91e75f22edf638897029c641bae3744cae
Sergey Okatov <sokatov@gmail.com>
2020-04-02 21:42:43 +0500

    work on backend-openapi integration. Fix: teams teable declaration in extensions.tf

--- crowdproj-back-app/src/main/kotlin/com/crowdproj/ktor/Server.kt
+++ crowdproj-back-app/src/main/kotlin/com/crowdproj/ktor/Server.kt
@@ -1,34 +1,26 @@
 package com.crowdproj.ktor
 
-import io.ktor.client.engine.apache.Apache
-import com.crowdproj.page.restProfiles
-import com.crowdproj.page.restTeams
-import io.kotless.dsl.ktor.Kotless
-import com.crowdproj.page.siteStatics
+//import io.ktor.html.respondHtml
+import com.crowdproj.ktor.page.siteStatics
+import com.crowdproj.ktor.storage.profile.restProfiles
+import com.crowdproj.ktor.storage.team.restTeams
 import com.crowdproj.rest.infrastructure.ApiKeyCredential
 import com.crowdproj.rest.infrastructure.ApiPrincipal
 import com.crowdproj.rest.infrastructure.apiKeyAuth
-import com.crowdproj.storage.TeamsStorage
 import com.typesafe.config.ConfigFactory
+import io.kotless.dsl.ktor.Kotless
 import io.ktor.application.Application
-import io.ktor.locations.Locations
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.auth.oauth
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
 import io.ktor.config.HoconApplicationConfig
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.GsonConverter
 import io.ktor.http.ContentType
-//import io.ktor.html.respondHtml
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
-import io.ktor.response.respondRedirect
-import io.ktor.response.respondText
-import io.ktor.routing.get
+import io.ktor.locations.Locations
 import io.ktor.routing.routing
-import org.apache.commons.validator.routines.UrlValidator
 import org.slf4j.LoggerFactory
 
 internal val settings = HoconApplicationConfig(ConfigFactory.defaultApplication(HTTP::class.java.classLoader))
@@ -71,37 +63,6 @@ class Server : Kotless() {
             siteStatics()
             restProfiles()
             restTeams()
-
-//            get("/") {
-//                call.respondHtml { main() }
-//            }
-
-            get("/r") {
-                val k = call.parameters["k"] ?: ""
-
-                val url = TeamsStorage.getByCode(k)
-                if (url == null) {
-                    call.respond(HttpStatusCode.NotFound, "Unknown short URL")
-                } else {
-                    call.respondRedirect(url)
-                }
-            }
-
-            get("/shorten") {
-                val value = call.parameters["value"] ?: ""
-
-                logger.info("URL for shortening $value")
-
-                val url = if (value.contains("://").not()) "https://$value" else value
-
-                if (UrlValidator.getInstance().isValid(url).not()) {
-                    call.respondText { "Non valid URL" }
-                } else {
-                    val code = TeamsStorage.getByUrl(url) ?: TeamsStorage.createCode(url)
-                    call.respondText { "https://ktor.short.kotless.io/r?k=$code" }
-                }
-            }
-
         }
     }
 }
